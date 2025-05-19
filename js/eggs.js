3ec98a5e063fe22be9c5dc39b5cdaa0e0b5c39ca
Nicolas Ouillon <59844605+Nico-ON-Web@users.noreply.github.com>
2025-05-19 15:17:49 +0200

    div id popup

--- js/eggs.js
+++ js/eggs.js
@@ -1,11 +1,14 @@
 const token= location.href.split("?token=")[1]
+
 if(token == "123456"){
     // Ajoute un badge
+    console.log("token 123456")
     pop(1)
     saveBadge(1)
 }else if(token=="maisquesquecestquecetruc"){
     pop(2)
     saveBadge(2)
 }
+
 
 
