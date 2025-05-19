481b0dfdd821eee290b638d1b0d439c4045d8c57
Nicolas Ouillon <59844605+Nico-ON-Web@users.noreply.github.com>
2025-05-19 09:24:41 +0200

    pop

--- js/eggs.js
+++ js/eggs.js
@@ -1,8 +1,10 @@
 const token= location.href.split("?token=")[1]
 if(token == "123456"){
     // Ajoute un badge
+    pop(1)
     saveBadge(1)
 }else if(token=="maisquesquecestquecetruc"){
+     pop(2)
     saveBadge(2)
 }
 
