af7464f946732e8e4fc1106e105d15b22f55cff6
Nicolas Ouillon <59844605+Nico-ON-Web@users.noreply.github.com>
2025-05-19 10:19:13 +0200

    classement

--- js/classement.js
+++ js/classement.js
@@ -1,1 +1,16 @@
-
+let classement = document.querySelector(".classement")
+fetch("./teams.json").then(rep=>rep.json()).then(data=>{
+    data.forEach(element => {
+        const participants = element.membres.join(", ")
+        classement.innerHTML += ` 
+        <div class="timeline">  
+                <div class="time">
+                    <div class="classement-avatar border">
+                        <p>${participants}</p>
+                        <h4><span>${element.score}</span>🏆</h4>
+                    </div>
+                    <div style="height: ${(element.score/15) * 500}px;" class="time-score"></div>       
+                </div>
+            </div>`
+    });
+})
